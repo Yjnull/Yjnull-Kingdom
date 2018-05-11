f59a7aceaf449d914e191ee85f658ff1281b80b8
yangya <759106909@qq.com>
2018-05-11 18:54:04 +0800

    finish Tree.java

--- project/Data_Algorithms/src/Chap08/Node.java
+++ project/Data_Algorithms/src/Chap08/Node.java
@@ -2,11 +2,15 @@ package Chap08;
 
 public class Node {
     int iData;         //关键字值
-    double fData;      //其他数据
+    double dData;      //其他数据
     Node leftChild;    //左子节点
     Node rightChild;   //右子节点
 
     public void displayNode() {
-
+        System.out.print('{');
+        System.out.print(iData);
+        System.out.print(", ");
+        System.out.print(dData);
+        System.out.print("} ");
     }
 }
